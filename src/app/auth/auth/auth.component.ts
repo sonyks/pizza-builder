8af298f0a8da47450ea00d5c4f10e72759bf1e3a
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2020-04-15 02:23:47 +0300

    added authenticated to recipe builder

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authFrom: NgForm) {
+    if (!authFrom.valid) {
+      return;
+    }
+
+    const email = authFrom.value.email;
+    const password = authFrom.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+    this.isLoading = true;
+    this.error = '';
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(result => {
+      console.log(result);
+      this.isLoading = false;
+    }, errorMessage => {
+      console.log(errorMessage);
+      this.error = errorMessage;     
+      this.isLoading = false;
+    });
+
+    authFrom.reset(); 
+  }
 }
